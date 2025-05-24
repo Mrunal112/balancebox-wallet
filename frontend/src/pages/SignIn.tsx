0d919ac0d3338750b6802f50bc53ee64fc4937c5
Mrunal Munjamkar <mrunalmunjamkar1@gmail.com>
2025-05-24 23:23:05 +0530

    SignIn Page Modified and Axios Setting updated

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,10 +1,29 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { LogIn, User, Lock, Eye, EyeOff } from "lucide-react";
+import { LogIn, User, Lock, Eye, EyeOff, Loader2 } from "lucide-react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import $axios from "@/lib/$axios";
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async () => {
+    setLoading(true);
+
+    try {
+      const response = $axios.post("/user/login", {
+        username,
+        password,
+      });
+    } catch (error) {
+      console.log("Login Failed", error);
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center px-4">
@@ -15,7 +34,12 @@ export default function SignIn() {
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <User className="h-4 w-4 text-gray-500" />
           </div>
-          <Input type="text" placeholder="Username" className="pl-10 w-full" />
+          <Input
+            type="text"
+            placeholder="Username"
+            className="pl-10 w-full"
+            onChange={(e) => setUserName(e.target.value)}
+          />
         </div>
 
         <div className="relative">
@@ -26,13 +50,14 @@ export default function SignIn() {
             type={showPassword ? "text" : "password"}
             placeholder="Password"
             className="pl-10 pr-10 w-full"
+            onChange={(e) => setPassword(e.target.value)}
           />
           <button
             type="button"
             onClick={() => {
               setShowPassword(!showPassword);
             }}
-            className="absolute inset-y-0 right-0 flex items-center pr-3"
+            className="absolute inset-y-0 right-0 flex items-center px-3"
           >
             {showPassword ? (
               <EyeOff className="h-4 w-4 text-gray-500 hover:text-gray-700" />
@@ -42,14 +67,36 @@ export default function SignIn() {
           </button>
         </div>
 
-        <Button
-          className="w-full flex items-center justify-center gap-2"
-          variant="outline"
-        >
-          Sign In
-          <LogIn className="h-4 w-4" />
-        </Button>
+        {loading ? (
+          <Button
+            disabled
+            className="w-full flex items-center justify-center gap-2"
+          >
+            <Loader2 className="h-4 w-4 animate-spin" />
+            Please wait
+          </Button>
+        ) : (
+          <Button
+            className="w-full flex items-center justify-center gap-2"
+            variant="outline"
+            disabled={!username || !password}
+            onClick={handleLogin}
+          >
+            Sign In
+            <LogIn className="h-4 w-4" />
+          </Button>
+        )}
+
+        <p className="text-center">
+          Don't have an account?{" "}
+          <Link className="text-blue-800 underline" to={"/"}>
+            Sign Up{" "}
+          </Link>
+        </p>
       </div>
     </div>
   );
 }
+
+// Popup appear if dosen't get login or user dosen't exists
+// Simplify the axios login
--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,7 +1,12 @@
+import { HomePage } from "@/pages/HomePage";
 import SignIn from "@/pages/SignIn";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
   {
     path: "/signin",
     element: <SignIn />,
