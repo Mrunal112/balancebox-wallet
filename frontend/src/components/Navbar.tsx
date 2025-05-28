e471708b10dddcda9a6444c8331f8eb38492714e
Mrunal Munjamkar <mrunalmunjamkar1@gmail.com>
2025-05-28 23:01:23 +0530

    Logout Functionality added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,18 +1,48 @@
 import { WalletMinimal } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
   return (
     <div className="flex justify-between items-center text-cyan-100 px-16 py-4 bg-stone-800">
       <div className="flex items-center text-2xl font-medium cursor-pointer gap-1">
         <WalletMinimal className="text-rose-300" /> BalanceBox-Wallet
       </div>
       <div className="flex text-xl font-small cursor-pointer gap-4">
-        Hello, User{" "}
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>U</AvatarFallback>
-        </Avatar>
+        Hello, User
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar className="cursor-pointer">
+              <AvatarImage
+                src="https://github.com/shadcn.png"
+                alt="User Profile"
+              />
+              <AvatarFallback>U</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="bg-stone-800 text-cyan-100 mt-3 mr-14 font-medium">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => {
+                localStorage.removeItem("token");
+                navigate("/signin");
+              }}
+            >
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
